e82e5625e7c7071c641626ba13e36692fbc1826d
moskvin <moskwin.a.va@gmail.com>
2018-03-31 14:13:37 +0300

    удалил std

--- src/videomeminfo.cpp
+++ src/videomeminfo.cpp
@@ -7,9 +7,9 @@
 //============================================================================
 
 #include <iostream>
-using namespace std;
+
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	std::cout << "!!!Hello World!!!" << std::endl; // prints !!!Hello World!!!
 	return 0;
 }
